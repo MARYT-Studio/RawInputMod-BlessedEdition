535ce4b7dc4b26ebc6d61db677c0dfd59ebee4a7
Isaac Thompson <risinginiris2017@gmail.com>
2024-01-11 15:57:52 +0800

    bump version

--- src/main/java/world/maryt/rawinput/RawInput.java
+++ src/main/java/world/maryt/rawinput/RawInput.java
@@ -20,7 +20,7 @@ public class RawInput
 {
     public static final String MODID = "rawinput";
 	public static final String NAME = "Raw Mouse Input";
-    public static final String VERSION = "1.6.1";
+    public static final String VERSION = "1.6.2";
 	public static final Logger LOGGER = LogManager.getLogger(NAME);
 
 
