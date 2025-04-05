1316c1949fc61c7cb7f9684d953c4bee8d57c5f4
Isaac Thompson <risinginiris2017@gmail.com>
2025-04-05 20:31:32 +0800

    Maybe it's ok

--- src/main/java/world/maryt/rawinput/RawInput.java
+++ src/main/java/world/maryt/rawinput/RawInput.java
@@ -16,18 +16,32 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = RawInput.MODID, version = RawInput.VERSION, name = RawInput.NAME, acceptedMinecraftVersions = "[1.12.2]")
+@Mod(modid = RawInput.MOD_ID, version = RawInput.VERSION, name = RawInput.MOD_NAME, acceptedMinecraftVersions = "[1.12.2]")
 public class RawInput
 {
-    public static final String MODID = "rawinput";
-	public static final String NAME = "Raw Mouse Input";
-    public static final String VERSION = "1.7.0";
-	public static final Logger LOGGER = LogManager.getLogger(NAME);
+    public static final String MOD_ID = Tags.MOD_ID;
+	public static final String MOD_NAME = "Raw Input Mod";
+    public static final String VERSION = Tags.VERSION;
+	public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 
+	@SideOnly(Side.CLIENT)
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		Config.load(event.getSuggestedConfigurationFile());
+		KeybindHandler.init();
+	}
 
 	@SideOnly(Side.CLIENT)
 	@EventHandler
     public void init(FMLInitializationEvent event) {
+		// If not enabled, this mod should register nothing.
+		if (!Config.enableModFunction) {
+			if (Config.disableReason.equals("forced")) LOGGER.info(MOD_NAME + "is disabled manually in config.");
+			if (Config.disableReason.equals("notWindows")) LOGGER.info(MOD_NAME + "detected your operating system is not Windows and disabled itself. If you need this mod, set \"iNeedThisMod\" to true.");
+			return;
+		}
+
+		LOGGER.info(MOD_NAME + "is enabled. Enjoy your smooth game control.");
 		ClientCommandHandler.instance.registerCommand(new RescanCommand());
 		ClientCommandHandler.instance.registerCommand(new ToggleCommand());
 		Minecraft.getMinecraft().mouseHelper = new RawMouseHelper();
@@ -36,11 +50,4 @@ public class RawInput
 
 		RawInputHandler.init();
     }
-
-	@SideOnly(Side.CLIENT)
-	@EventHandler
-	public void preInit(FMLPreInitializationEvent event) {
-		Config.load(event.getSuggestedConfigurationFile());
-		KeybindHandler.init();
-	}
 }
--- src/main/java/world/maryt/rawinput/config/Config.java
+++ src/main/java/world/maryt/rawinput/config/Config.java
@@ -12,9 +12,12 @@ public class Config {
     private static Configuration latestConfig;
 
     public static Set<String> guiBlacklist = Sets.newHashSet();
+    public static boolean enableModFunction;
+    public static String disableReason;
 
     private Config() {}
 
+    @SuppressWarnings("all")
     public static void load(File file) {
         latestConfig = new Configuration(file);
         latestConfig.load();
@@ -24,7 +27,39 @@ public class Config {
     }
 
     public static void loadConfig() {
-        String[] list = latestConfig.getStringList("guiBlackList", Configuration.CATEGORY_CLIENT, new String[]{"hellfirepvp.astralsorcery.client.gui.GuiObservatory"}, "All GUIs whose class names are in this list will disable RawInput.");
+        // GUI Blacklist
+        String[] list = latestConfig.getStringList("guiBlackList",
+                Configuration.CATEGORY_CLIENT,
+                new String[]{"hellfirepvp.astralsorcery.client.gui.GuiObservatory"},
+                "All GUIs whose class names are in this list will disable RawInput.");
         if (list.length > 0) {guiBlacklist = Sets.newHashSet(list);}
+
+        // Operating System Check
+        String enable = latestConfig.getString("iNeedThisMod",
+                Configuration.CATEGORY_CLIENT,
+                "auto",
+                "Here are three options:\n" +
+                        "\"true\": force entire functionality of mod to be enabled.\n" +
+                        "\"false\": force entire functionality of mod to be disabled.\n" +
+                        "\"auto\": let the mod detect if your operating system is Windows, and disable the mod if not.\n" +
+                        "Anything other input than the three options above will be seen as default value \"auto\".\n" +
+                        "Only change this when you know what you are doing.");
+        switch (enable) {
+            case "true": {
+                enableModFunction = true;
+                disableReason = "";
+                break;
+            }
+            case "false": {
+                enableModFunction = false;
+                disableReason = "forced";
+                break;
+            }
+            default: {
+                enableModFunction = System.getProperty("os.name").toLowerCase().contains("windows");
+                disableReason = "notWindows";
+                break;
+            }
+        }
     }
 }
